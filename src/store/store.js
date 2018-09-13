512777b8de14d5dba72e9c395705a5ff81042dd6
big-MuMu <13564607847@163.com>
2018-09-13 18:16:25 +0800

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,46 +4,31 @@ import lazyLoading from '../assets/lazy'
 Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
-        items: [],
-        isLoadRoutes: false
-    },
-
-    getters: {
-        menuitems: state => {
-            return state.items
-        },
-        isLoadRoutes: state => {
-            return state.isLoadRoutes
-        }
+        menu: [],
+        // isLoadRoutes: false
     },
     mutations: {
-        addMenu(state, menuItems) {
-            if (menuItems.length === 0) {
-                state.items = []
-            } else {
-                // state.items = menuItems
-                menuItems.map(function(item) {
-                    item.component = lazyLoading(item.component)
-                    item.children.map(function(child) {
-                        child.component = lazyLoading(child.component)
-                    })
+        addMenu(state, menuItems = []) {
+            if (!menuItems.length) {
+                state.menu = [];
+                return
+            }
+            // 后台传来的menu参数是字符串 所以要把component处理一下
+            menuItems.forEach(i => {
+                i.component = lazyLoading(i.component)
+                i.children.forEach((child) => {
+                    // console.log(child.component)
+                    child.component = lazyLoading(child.component)
+                    // console.log(child.component)
                 })
-                state.items.push(...menuItems)
-            }
+            })
+            // this.$router.addRoutes(menuItems)
+            // state.items.push(...menuItems)
+            state.menu = [...state.menu, ...menuItems];
         },
-        loadRoutes(state) {
+        /* loadRoutes(state) {
             state.isLoadRoutes = !state.isLoadRoutes
-        }
-    },
-    actions: {
-        addMenu({ commit }, menuItems) {
-            if (menuItems.length > 0) {
-                commit('addMenu', menuItems)
-            }
-        },
-        loadRoutes({ commit }) {
-            commit('loadRoutes')
-        }
+        } */
     }
 })
 
